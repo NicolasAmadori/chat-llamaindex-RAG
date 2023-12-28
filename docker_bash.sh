7fb2e0e9e739e2ec23b3abfb291030267adbcabc
StefanoCoamonaco <stef_2000@hotmail.it>
2023-12-28 12:33:35 +0100

    fixed docker bash

--- docker_bash.sh
+++ docker_bash.sh
@@ -5,29 +5,29 @@ if [ $# -lt 1 ]; then
 fi
 
 case $1 in
-    "run")
+    run)
         # Run the docker container
         docker run -v ./:/src/ --rm --gpus device=$CUDA_VISIBLE_DEVICES -d -it -p 37331:37331 -p 37333:37333 -p 19530:19530 --name test-llamaindex-container test-llamaindex
         ;;
-    "exec")
+    exec)
         # Execute the models inside the docker container
         docker exec -it test-llamaindex-container bash      
         ;;
-    "build")
+    build)
         # Build the docker
         docker build ./ -t test-llamaindex
         ;;
-    "stop")
+    stop)
         # Stop the docker container
         docker stop test-llamaindex-container
         ;;
-    "remove")
+    remove)
         # Remove the docker container
         docker stop test-llamaindex-container &&
         docker remove test-llamaindex-container
         ;;
-    "*")
+    *)
         # Invalid argument
-        Echo "Invalid argument"
+        echo "Invalid argument"
         ;;
 esac
